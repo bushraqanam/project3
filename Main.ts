277d8b0d1ff84f4f067c14fe8165f96685675e47
bushraqanam <bqanam@gmail.com>
2024-08-06 21:12:19 -0500

    Add files via upload

---
+++ Main.ts
@@ -0,0 +1,50 @@
+let a = 5;
+console.log(a)
+//this is a simple my name program
+let myName = "BushraQanam"
+console.log (myName)
+myName = "alina sahrish"
+console.log(myName)
+myName = "aairah inshrah"
+console.log(myName)
+myName = "aizah zainab"
+console.log(myName)
+myName = "amaan ahmed"
+console.log(myName)
+myName = "numa noorin"
+console.log(myName)
+//this is a simple my age program
+let my_age = 40
+console.log(my_age)
+ my_age = 14
+ console.log(my_age)
+ my_age = 8
+ console.log(my_age)
+ my_age = 7
+ console.log(my_age)
+ my_age = 18
+ console.log(my_age)
+ my_age = 20
+ console.log(my_age)
+ //this is a simple my cars program
+ let MyCars = true
+ console.log (MyCars)
+ MyCars = false
+ console.log(MyCars)
+ MyCars = true
+ console.log(MyCars)
+ MyCars = false
+ console.log(MyCars)
+ MyCars = true
+ console.log(MyCars)
+ let myArea = "hello world"
+ console.log(myArea)
+ let number = 10
+ console.log(number) 
+ number = 20
+ console.log(number)
+ let myBooks = "bookcaes"
+ console.log (myBooks)
+ 
+ 
+ 
